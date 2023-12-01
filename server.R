b20024fd2fbd367bfa814232252f671bb30809db
sarahpagan <sdpagan@ncsu.edu>
2023-12-01 00:17:08 -0500

    Prediction tab build

--- server.R
+++ server.R
@@ -1,9 +1,11 @@
 library(shiny)
 library(caret)
+library(forcats)
 
 function(input, output, session) {
  
-    ### Model Fitting   
+######## Model fitting   
+    
     output$split <- renderText({
         paste0(input$slider, "% of the data is allocated to training")
     })
@@ -12,37 +14,143 @@ function(input, output, session) {
         createDataPartition(data$Cancer, p = input$slider/100, list = FALSE)
     })
     
-    trainData <- eventReactive(input$button, {
+    train_data <- eventReactive(input$button, {
         data[index(), ]
     })
     
-    testData <- eventReactive(input$button, {
+    test_data <- eventReactive(input$button, {
         data[-index(), ]
     })
     
-    vars <- eventReactive(input$button, {
-        input$multi
+    logit_title <- eventReactive(input$button, {
+        "Logistic Regression Model"
     })
     
-    mtry <- eventReactive(input$range, {
-        input$range
+    logit_fit <- eventReactive(input$button, {
+        formula <- paste("Cancer", collpase = "~", 
+                         paste(input$multi, collapse = "+"))
+        glm(formula, data = train_data(), family = binomial)
+    })
+    
+    rf_title <- eventReactive(input$button, {
+        "Random Forest Model"
+    })
+    
+    rf_fit <- eventReactive(input$button, {
+        mtry <- input$range
+        train(Cancer ~ ., data = train_data(),
+              method = "rf",
+              family = "binomial",
+              metric = "Accuracy",
+              trControl = trainControl(method = "cv",
+                                       number = input$cvfolds),
+              tuneGrid = data.frame(mtry = mtry[1]:mtry[2]))
+    })
+    
+    logit_test_title <- eventReactive(input$button, {
+        "Logistic Regression Test Results"
+    })
+    
+    logit_test <- eventReactive(input$button, {
+        test <- test_data()
+        preds <- predict(logit_fit(), newdata = test, type = "response")
+        classify <- ifelse(preds > 0.5, 1, 0)
+        confusionMatrix(as_factor(classify), test$Cancer)
+    })
+    
+    output$glmt <- renderText({
+        logit_title()
     })
     
     output$glm <- renderPrint({
-        formula <- paste("Cancer", collpase = "~", 
-                         paste(vars(), collapse = "+"))
-        logitfit <- glm(formula, data = trainData(), family = binomial)
-        summary(logitfit)
+        logit_results <- logit_fit()
+        summary(logit_results)
+    })
+    
+    output$rft <- renderText({
+        rf_title()
     })
     
     output$rf  <-  renderPrint({
-        rf_fit <- train(Cancer ~ ., data = trainData(),
-                       method = "rf",
-                       family = "binomial",
-                       metric = "Accuracy",
-                       trControl = trainControl(method = "cv",
-                                                number = 5),
-                       tuneGrid = data.frame(mtry = mtry()[1]:mtry()[2]))
-        list(rf_fit$results, confusionMatrix(rf_fit))
+        rf_fit()
+    })
+    
+    output$glm_test_title <- renderText({
+        logit_test_title()
+    })
+    
+    output$glm_test <- renderPrint({
+        logit_test()
+    })
+    
+####################
+    
+    
+######## Prediction
+    
+    new <- eventReactive(input$predict, {
+        data.frame(
+            Age = input$age,
+            Partners = input$partners,
+            First = input$first,
+            Pregnancies = input$preg,
+            Smokes = as_factor(input$smokes),
+            Smokes_years = input$smokeyrs,
+            Contraceptives = as_factor(input$cont),
+            Contraceptives_years = input$contyrs,
+            IUD = as_factor(input$iud),
+            IUD_years = input$iudyrs,
+            STDs = as_factor(input$stds),
+            STDs_number = input$stdsyrs,
+            STD_condylomatosis = as_factor(input$c),
+            STD_vulvo_perineal_condylomatosis = as_factor(input$vpc),
+            STD_syphilis = as_factor(input$s),
+            STD_genital_herpes = as_factor(input$gh),
+            STD_molluscum_contagiosum = as_factor(input$mc),
+            STD_HIV = as_factor(input$hiv),
+            STD_HPV = as_factor(input$hpv),
+            Dx_Cancer = as_factor(input$c),
+            Dx_CIN = as_factor(input$cin),
+            Dx_HPV = as_factor(input$dhpv))
+    })
+    
+    glmp_title <- eventReactive(input$predict, {
+        "Logistic Regression Model Predction"
+    })
+    
+    sub_title <- eventReactive(input$predict, {
+        "Probability of cervical cancer:"
+    })
+    
+    rfp_title <- eventReactive(input$predict, {
+        "Random Forest Model Predction"
+    })
+    
+    sub_title2 <- eventReactive(input$predict, {
+        "Probability of cervical cancer:"
+    })
+    
+    output$glmpt <- renderText({
+        glmp_title()
+    })
+    
+    output$sub <- renderText({
+        sub_title()
+    })
+    
+    output$rfpt <- renderText({
+        rfp_title()
+    })
+    
+    output$sub2 <- renderText({
+        sub_title2()
+    })
+    
+    output$glm_pred <- renderPrint({
+        predict(logit_fit(), newdata = new(), type = "response")
+    })
+    
+    output$rf_pred <- renderPrint({
+        predict(rf_fit(), newdata = new(), type = "prob")
     })
 }
--- ui.R
+++ ui.R
@@ -7,10 +7,11 @@ library(shinyWidgets)
 ### Read in Data and re-code Boolean variables as factors
 data <- read_csv("CC.csv") |>
     mutate(Smokes = as_factor(Smokes),
+           Contraceptives = as_factor(Contraceptives),
            IUD = as_factor(IUD),
            STDs = as_factor(STDs),
            STD_condylomatosis = as_factor(STD_condylomatosis),
-           STD_cervical_condlomatosis = as_factor(STD_cervical_condlomatosis),
+           STD_cervical_condylomatosis = as_factor(STD_cervical_condylomatosis),
            STD_vaginal_condylomatosis = as_factor(STD_vaginal_condylomatosis),
            STD_vulvo_perineal_condylomatosis = as_factor(STD_vulvo_perineal_condylomatosis),
            STD_syphilis = as_factor(STD_syphilis),
@@ -26,9 +27,9 @@ data <- read_csv("CC.csv") |>
            Dx_HPV = as_factor(Dx_HPV),
            Cancer = as_factor(Cancer)) |>
     ### Drop factors with only one level (no observations)
-    select(-STD_cervical_condlomatosis,
+    select(-STD_cervical_condylomatosis,
+           -STD_vaginal_condylomatosis,
            -STD_pelvic_inflammatory_disease,
-           -STD_molluscum_contagiosum,
            -STD_AIDS,
            -STD_HepatitisB)
 
@@ -36,9 +37,9 @@ data <- read_csv("CC.csv") |>
 variables <- c("Age", "Partners", "First", "Pregnancies", "Smokes",
                "Smokes_years", "Contraceptives", "Contraceptives_years",
                "IUD", "IUD_years", "STDs", "STDs_number", "STD_condylomatosis",
-               "STD_vaginal_condylomatosis", "STD_vulvo_perineal_condylomatosis",
-               "STD_syphilis", "STD_genital_herpes", "STD_HIV", "STD_HPV",
-               "Dx_Cancer", "Dx_CIN", "Dx_HPV") 
+               "STD_vulvo_perineal_condylomatosis", "STD_syphilis",
+               "STD_genital_herpes", "STD_molluscum_contagiosum", "STD_HIV",
+               "STD_HPV", "Dx_Cancer", "Dx_CIN", "Dx_HPV") 
 
 fluidPage(
     
@@ -51,6 +52,8 @@ fluidPage(
                  
                  tabsetPanel(
                      tabPanel("Model Info"),
+                     
+                     ### Model fitting
                      tabPanel("Model Fitting",
                               sidebarPanel(
                                   sliderInput("slider",
@@ -73,15 +76,92 @@ fluidPage(
                                                     value = c(1, 5),
                                                     min = 1,
                                                     max = 10),
+                                  numericInput("cvfolds",
+                                               "Select Number of CV Folds",
+                                                    value = 5,
+                                                    min = 2,
+                                                    max = 10),
+                                  br(),
                                   actionButton("button", "Run Models")),
                               mainPanel(
-                                  h2("Logistic Regression Results"),
+                                  h3(textOutput("glmt")),
                                   verbatimTextOutput("glm"),
-                                  h2("Random Forest Results"),
-                                  verbatimTextOutput("rf")
-                              )),
-                     tabPanel("Prediction")
+                                  h3(textOutput("rft")),
+                                  verbatimTextOutput("rf"),
+                                  fluidRow(
+                                      column(6,
+                                             h3(textOutput("glm_test_title")),
+                                             verbatimTextOutput("glm_test"))))),
+                                      
+                     ### Prediction
+                     tabPanel("Prediction",
+                              h3("Enter predictor values below to make a prediction"),
+                              h4("Note: yes = 1 and no = 0"),
+                              br(),
+                              fluidRow(
+                                  column(3,
+                                         numericInput("age", "Age",
+                                                      value = 50, min = 1, max = 100),
+                                         numericInput("partners", "Number of sexual partners",
+                                                      value = 0, min = 0, max = 100),
+                                         numericInput("first", "Age of first sexual intercourse",
+                                                      value = 0, min = 0, max = 100),
+                                         numericInput("preg", "Number of pregnancies",
+                                                      value = 0, min = 0, max = 100),
+                                         numericInput("smokes", "Smokes",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("smokeyrs", "Years of smoking",
+                                                      value = 0, min = 0, max = 100)),
+                                  column(3, 
+                                         numericInput("cont", "Use of hormonal contraceptives",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("contyrs", "Years of hormonal contraceptive use",
+                                                      value = 0, min = 0, max = 100),
+                                         numericInput("iud", "Use of IUD",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("iudyrs", "Years with IUD",
+                                                      value = 0, min = 0, max = 100),
+                                         numericInput("stds", "Any STDs",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("stdsyrs", "Number of STDs",
+                                                      value = 0, min = 0, max = 10)),
+                                  column(3,
+                                         numericInput("c", "Condylomatosis",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("vpc", "Vulvo Perineal Condylomatosis",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("s", "Syphilis",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("gh", "Genital Herpes",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("mc", "Molluscum Contagiosum",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("hiv", "HIV",
+                                                      value = 0, min = 0, max = 1)),    
+                                  column(3, 
+                                         numericInput("hpv", "HPV",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("c", "Ever diagnosed with cancer",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("cin", "Ever diagnosed with CIN",
+                                                      value = 0, min = 0, max = 1),
+                                         numericInput("dhpv", "Ever diagnosed with HPV",
+                                                      value = 0, min = 0, max = 1),
+                                         br(),
+                                         actionButton("predict", "Make Prediction"))),
+                              
+                              fluidRow(
+                                  column(6,
+                                         h3(textOutput("glmpt")),
+                                         h5(textOutput("sub")),
+                                         verbatimTextOutput("glm_pred")),
+                                  column(6,
+                                         h3(textOutput("rfpt")),
+                                         h5(textOutput("sub2")),
+                                         verbatimTextOutput("rf_pred")))
+                     )
                  )
         )
     )
 )
+                                        
